1677afd57b5a1af4c8045be3862809633ded3474
Huy Hoang <huy.hoang711@gmail.com>
2017-04-09 14:22:59 -0500

    .

--- myavg.cpp
+++ myavg.cpp
@@ -38,14 +38,14 @@ class SumEtCountFactory : public TransformFunctionFactory {
 		returnType.addFloat();
 		returnType.addInt();
 	}
-	/*
+
 	virtual void getReturnType(ServerInterface &srvInterface,
 		const SizedColumnTypes &input_types,
-    		SizedColumnTypes &output_types) {
+		SizedColumnTypes &output_types) {
 		output_types.addFloat("The Sum");
 		output_types.addInt("The Count");
 	}
-	*/
+
 	virtual TransformFunction *createTransformFunction(ServerInterface &srvInterface) {
 		return vt_createFuncObj(srvInterface.allocator, SumEtCount);
 	}
